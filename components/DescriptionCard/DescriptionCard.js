cc74525a98b5f278437797ff20476b85e1d4ae8e
DarwinLozada <darwinlozadadev@gmail.com>
2021-04-28 01:12:51 -0400

    done description card

--- components/DescriptionCard/DescriptionCard.js
+++ components/DescriptionCard/DescriptionCard.js
@@ -5,13 +5,13 @@ import { DescriptionIcon } from "../SvgIcons/SvgIcons"
 export default function DescriptionCard({ petName, petDescription }) {
   return (
     <div
-      className="grid gap-6 pet-info-section"
+      className="grid gap-6 bg-softPink mx-8 px-6 py-8 rounded-card"
       style={{ gridTemplateColumns: "auto auto" }}
     >
-      <DescriptionIcon className="w-8 text-primaryWhite" />
-      <section>
-        <h3>{`More info about ${petName}`}</h3>
-        <p>{petDescription}</p>
+      <DescriptionIcon className="w-8 text-mediumPink" />
+      <section className="flex flex-col gap-6">
+        <h3 className="text-lg font-semibold text-hardPink">{`More info about ${petName}`}</h3>
+        <p className="text-[#A7224E] font-medium">{petDescription}</p>
       </section>
     </div>
   )
