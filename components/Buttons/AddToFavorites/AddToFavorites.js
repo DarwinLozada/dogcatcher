c6f186aaf5ff7b903e01c03cc21332c3f282acdb
DarwinLozada <darwinlozadadev@gmail.com>
2021-04-28 22:59:24 -0400

    add spinner to add to favorites button

--- components/Buttons/AddToFavorites/AddToFavorites.js
+++ components/Buttons/AddToFavorites/AddToFavorites.js
@@ -1,35 +1,44 @@
 // Icon Components
 import { PlusIcon } from "../../SvgIcons/SvgIcons"
 
+// Components
+import Spinner from "../../Spinner/Spinner"
+
 // Dependencies
 import { addPetToFavorites } from "../../../firebase-services/database"
+import { useState, useCallback } from "react"
 import useUser from "../../../stores/UserStore"
 import useToast from "../../../stores/ToastsStore"
 
 export default function AddToFavorites({ petInfo }) {
   const [user] = useUser()
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false)
 
   const toast = useToast()
 
-  const handleClick = () => {
+  const handleClick = useCallback(() => {
+    setIsButtonDisabled(true)
     addPetToFavorites(petInfo, user.uid)
-      .then(() =>
+      .then(() => {
         toast(
           "success",
           "Pet added succesfully",
           `${petInfo.name} is now in your favorites`
         )
-      )
+
+        setIsButtonDisabled(false)
+      })
       .catch((err) => console.log(err))
-  }
+  }, [])
 
   return (
     <button
-      className={`flex transition-all duration-300 items-center gap-2 px-2 text-xs py-3 text-white rounded-md font-bold bg-mediumPink hover:bg-hardPink outline-none focus:outline-none focus:ring-2 ring-white`}
+      className={`flex transition-all duration-300 items-center gap-2 px-2 text-xs py-3 text-white rounded-md font-bold bg-mediumPink disabled:bg-lightBrown disabled:text-mediumPink hover:bg-hardPink outline-none focus:outline-none focus:ring-2 ring-white`}
       onClick={handleClick}
+      disabled={isButtonDisabled}
     >
       Add to favorites
-      <PlusIcon className="w-4" />
+      {isButtonDisabled ? <Spinner /> : <PlusIcon className="w-4" />}
     </button>
   )
 }
---
+++ components/Spinner/Spinner.js
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="animate animate-spin w-4 h-4 border-4 border-softPink rounded-full spinner-border">
+      <div></div>
+    </div>
+  )
+}
