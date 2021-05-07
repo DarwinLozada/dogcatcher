95aea5fcdc617b9352b874f3cab4c9951a9c286c
DarwinLozada <darwinlozadadev@gmail.com>
2021-05-07 18:22:26 -0400

    changed icons

--- components/MobileNav/MobileNav.js
+++ components/MobileNav/MobileNav.js
@@ -5,6 +5,7 @@ import UserAvatar from "../UserAvatar/UserAvatar"
 // Icon Components
 import {
   HomeIcon,
+  DiscoverPetsIcon,
   ProfileIcon,
   SettingsIcon,
   HeartIcon,
@@ -17,13 +18,12 @@ export default function MobileNav() {
   const { user } = useUser()
 
   return (
-    <div className="flex px-6 py-3 gap-20 items-center justify-center fixed bottom-0 right-0 z-100 w-full bg-primaryWhite">
-      <HomeIcon className="w-7" />
+    <div className="flex px-8 py-3 gap-20 items-center justify-center fixed bottom-0 right-0 z-100 w-full bg-primaryWhite">
+      <DiscoverPetsIcon className="w-8" />
       {user === undefined && <Spinner width={"8"} />}
       {user === null && <ProfileIcon className="w-8" />}
       {user && <UserAvatar avatarURL={user.avatar} username={user.name} />}
-      <SettingsIcon className="w-8" />
-      <HeartIcon className="w-7 text-hardPink stroke-current stroke-1" />
+      <HeartIcon className="w-7 text-hardPink stroke-current stroke-[1.3px]" />
     </div>
   )
 }
