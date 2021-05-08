d7a63eaf5ff95060346b907fe33a2a3818d0d7e6
DarwinLozada <darwinlozadadev@gmail.com>
2021-05-08 16:35:36 -0400

    added bottom padding in page layout

--- components/PageLayout/PageLayout.js
+++ components/PageLayout/PageLayout.js
@@ -11,7 +11,7 @@ export default function PageLayout({ children }) {
     <ToastContainer>
       <div id="modal-container"></div>
       <ModalContainer>
-        <div className="relative flex flex-col bg-lightBrown min-h-screen">
+        <div className="relative flex flex-col bg-lightBrown min-h-screen pb-24">
           <Header />
           <div className="mx-6 mt-12">{children}</div>
           <MobileNav />
