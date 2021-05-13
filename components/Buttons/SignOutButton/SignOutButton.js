bb149b79a15cc7f9e86fe830c170a71e1dd61721
DarwinLozada <darwinlozadadev@gmail.com>
2021-05-13 02:37:11 -0400

    added toast feedback when the user signs out

--- components/Buttons/SignOutButton/SignOutButton.js
+++ components/Buttons/SignOutButton/SignOutButton.js
@@ -1,12 +1,25 @@
 // Dependencies
 import useSignOut from "../../../hooks/useSignOut"
+import useToast from "../../../stores/ToastsStore"
 
 export default function SignOutButton() {
   const { isSignOutLoading, signOutError, signOut } = useSignOut()
 
+  const toast = useToast()
+
+  const handleClick = () => {
+    signOut()
+    toast(
+      "succesful",
+      "You have signed out succesfuly",
+      "Now you won't be able to add pets :("
+    )
+  }
+
   return (
     <button
       disabled={isSignOutLoading}
+      onClick={handleClick}
       className="transition-all duration-300 text-primaryWhite rounded-md text-sm bg-mediumPink px-5 py-2 dark:bg-hardPink dark:text-primaryWhite hover:bg-hardPink dark:hover:bg-mediumPink outline-none focus:outline-none focus:ring-2 ring-softPink"
     >
       SignOut
