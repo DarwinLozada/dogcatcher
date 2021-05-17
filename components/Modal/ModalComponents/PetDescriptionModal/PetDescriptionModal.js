a83698172d9175d48b934e517c7f50c9e519d20f
DarwinLozada <darwinlozadadev@gmail.com>
2021-05-16 23:15:36 -0400

    added dark styles for pet info modal

--- components/Modal/ModalComponents/PetDescriptionModal/PetDescriptionModal.js
+++ components/Modal/ModalComponents/PetDescriptionModal/PetDescriptionModal.js
@@ -6,13 +6,15 @@ export default function PetDescriptionModal(props) {
 
   return (
     <div
-      className="grid gap-6 bg-softPink mx-8 px-6 py-8 rounded-card"
+      className="grid gap-6 bg-softPink dark:pet-card-dark-gradient mx-8 px-6 py-8 rounded-card"
       style={{ gridTemplateColumns: "auto auto" }}
     >
       <DescriptionIcon className="w-8 text-mediumPink" />
       <section className="flex flex-col gap-6">
-        <h3 className="text-lg font-semibold text-hardPink">{`More info about ${petName}`}</h3>
-        <p className="text-redWine font-medium">{petDescription}</p>
+        <h3 className="text-lg font-semibold text-hardPink dark:text-primaryWhite">{`More info about ${petName}`}</h3>
+        <p className="text-redWine font-medium dark:text-hardPink />
+          {petDescription}
+        </p>
         <a
           target="_blank"
           aria-label={`${petName} Wikipedia article`}
